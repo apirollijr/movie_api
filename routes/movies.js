7074b110eeef5193b8947ef1dcc35b021a35b870
Anthony Pirolli Jr <apirollijr@gmail.com>
2025-07-01 20:19:02 -0400

    testing movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,33 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const Movie = require('../models/movie');
-const User = require('../models/user');
+// const express = require('express');
+// const router = express.Router();
+// const passport = require('passport');
+// const Movie = require('../models/movie');
+// const User = require('../models/user');
 
-const authenticate = passport.authenticate('jwt', { session: false });
+// const authenticate = passport.authenticate('jwt', { session: false });
 
-router.get('/', authenticate, async (req, res) => {
+// router.get('/', authenticate, async (req, res) => {
+//   try {
+//     const movies = await Movie.find();
+//     res.json(movies);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
+
+// router.get('/:title', authenticate, async (req, res) => {
+//   try {
+//     const movie = await Movie.findOne({ Title: req.params.title });
+//     if (!movie) return res.status(404).send('Movie not found');
+//     res.json(movie);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
+
+// module.exports = router;
+
+router.get('/', async (req, res) => {
   try {
     const movies = await Movie.find();
     res.json(movies);
@@ -15,14 +36,3 @@ router.get('/', authenticate, async (req, res) => {
   }
 });
 
-router.get('/:title', authenticate, async (req, res) => {
-  try {
-    const movie = await Movie.findOne({ Title: req.params.title });
-    if (!movie) return res.status(404).send('Movie not found');
-    res.json(movie);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-module.exports = router;
